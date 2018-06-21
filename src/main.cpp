fb44432ce758bdde33021e1c3293e2fd8f73e392
Alvaro Ariza Machado <adjam.10@outlook.com>
2018-06-21 16:02:00 -0500

    conexion a red local: Succesed

--- src/main.cpp
+++ src/main.cpp
@@ -1,110 +1,88 @@
-#include <Arduino.h>
-//Codigo no hecho por mi
-byte statusLed    = 13;
+#include <ESP8266WiFi.h>
 
-uint8_t sensorPin = 4;  // 0 = digital pin 2
-//byte sensorPin       = 2;
+const char* ssid     = "SEMARD";
+const char* password = "SEMARD123";
 
-// The hall-effect flow sensor outputs approximately 4.5 pulses per second per
-// litre/minute of flow.
-float calibrationFactor = 4.5;
+const char* host = "data.sparkfun.com";
+const char* streamId   = "....................";
+const char* privateKey = "....................";
 
-volatile byte pulseCount;
+void setup() {
+  Serial.begin(115200);
+  delay(10);
 
-float flowRate;
-unsigned int flowMilliLitres;
-unsigned long totalMilliLitres;
+  // We start by connecting to a WiFi network
 
-unsigned long oldTime;
-void pulseCounter()
-{
-  // Increment the pulse counter
-  pulseCount++;
-}
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
 
-void setup()
-{
+  /* Explicitly set the ESP8266 to be a WiFi-client, otherwise, it by default,
+     would try to act as both a client and an access-point and could cause
+     network-issues with your other WiFi-devices on your WiFi-network. */
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
 
-  // Initialize a serial connection for reporting values to the host
-  Serial.begin(9600);
-
-  // Set up the status LED line as an output
-  pinMode(statusLed, OUTPUT);
-  digitalWrite(statusLed, HIGH);  // We have an active-low LED attached
-
-  pinMode(sensorPin, INPUT);
-  digitalWrite(sensorPin, HIGH);
-
-  pulseCount        = 0;
-  flowRate          = 0.0;
-  flowMilliLitres   = 0;
-  totalMilliLitres  = 0;
-  oldTime           = 0;
-
-  // The Hall-effect sensor is connected to pin 2 which uses interrupt 0.
-  // Configured to trigger on a FALLING state change (transition from HIGH
-  // state to LOW state)
-  attachInterrupt(digitalPinToInterrupt(sensorPin), pulseCounter, FALLING);
-}
-
-/**
- * Main program loop
- */
-void loop()
-{
-
-   if((millis() - oldTime) > 1000)    // Only process counters once per second
-  {
-    // Disable the interrupt while calculating flow rate and sending the value to
-    // the host
-    detachInterrupt(digitalPinToInterrupt(sensorPin));
-
-    // Because this loop may not complete in exactly 1 second intervals we calculate
-    // the number of milliseconds that have passed since the last execution and use
-    // that to scale the output. We also apply the calibrationFactor to scale the output
-    // based on the number of pulses per second per units of measure (litres/minute in
-    // this case) coming from the sensor.
-    flowRate = ((1000.0 / (millis() - oldTime)) * pulseCount) / calibrationFactor;
-
-    // Note the time this processing pass was executed. Note that because we've
-    // disabled interrupts the millis() function won't actually be incrementing right
-    // at this point, but it will still return the value it was set to just before
-    // interrupts went away.
-    oldTime = millis();
-
-    // Divide the flow rate in litres/minute by 60 to determine how many litres have
-    // passed through the sensor in this 1 second interval, then multiply by 1000 to
-    // convert to millilitres.
-    flowMilliLitres = (flowRate / 60) * 1000;
-
-    // Add the millilitres passed in this second to the cumulative total
-    totalMilliLitres += flowMilliLitres;
-
-    unsigned int frac;
-
-    // Print the flow rate for this second in litres / minute
-    Serial.print("Flow rate: ");
-    Serial.print(int(flowRate));  // Print the integer part of the variable
-    Serial.print("L/min");
-    Serial.print("\t"); 		  // Print tab space
-
-    // Print the cumulative total of litres flowed since starting
-    Serial.print("Output Liquid Quantity: ");
-    Serial.print(totalMilliLitres);
-    Serial.println("mL");
-    Serial.print("\t"); 		  // Print tab space
-	Serial.print(totalMilliLitres/1000);
-	Serial.print("L");
-
-
-    // Reset the pulse counter so we can start incrementing again
-    pulseCount = 0;
-
-    // Enable the interrupt again now that we've finished sending output
-    attachInterrupt(digitalPinToInterrupt(sensorPin), pulseCounter, FALLING);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
   }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
-/*
-Insterrupt Service Routine
- */
+int value = 0;
+
+void loop() {
+  delay(5000);
+  ++value;
+
+  Serial.print("connecting to ");
+  Serial.println(host);
+
+  // Use WiFiClient class to create TCP connections
+  WiFiClient client;
+  const int httpPort = 80;
+  if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+
+  // We now create a URI for the request
+  String url = "/input/";
+  url += streamId;
+  url += "?private_key=";
+  url += privateKey;
+  url += "&value=";
+  url += value;
+
+  Serial.print("Requesting URL: ");
+  Serial.println(url);
+
+  // This will send the request to the server
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" +
+               "Connection: close\r\n\r\n");
+  unsigned long timeout = millis();
+  while (client.available() == 0) {
+    if (millis() - timeout > 5000) {
+      Serial.println(">>> Client Timeout !");
+      client.stop();
+      return;
+    }
+  }
+
+  // Read all the lines of the reply from server and print them to Serial
+  while(client.available()){
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
+
+  Serial.println();
+  Serial.println("cl
+  osing connection");
+}
